ccbf1844d62bdcbe2efcc02ccdf30c7eaf38ce65
ahmad abdulazeez <ahmadabdulazeez354@gmail.com>
2022-10-28 00:27:53 +0100

    feat: navbar --fix

--- components/Layout.js
+++ components/Layout.js
@@ -2,7 +2,7 @@ import Navbar from "./Navbar"
 
 const Layout = ({ children }) => {
     return (
-      <div>
+      <div className="grid grid-cols-[17rem_1fr]">
         <Navbar />
         {children}
       </div>
--- components/Navbar.js
+++ components/Navbar.js
@@ -5,33 +5,43 @@ import Link from "next/link"
 
 const Navbar = () => {
     return (
-        <div className="bg-blue-700 h-screen w-1/5 float-left text-white">
+        <div className="bg-blue-700 h-screen w-full text-white">
             <div className="flex mx-4 my-6">
                 <Image src="/icons8.png" width={40} height={40} className="" />
                 <h2 className="text-2xl font-semibold mx-4 mt-1">Xen</h2>
             </div>
 
             <nav>
-                <div className="p-4 hover:bg-blue-800 w-60 ml-4 rounded flex">
-                    <Image src="/dashboard.svg" width={15} height={15} />
-                    <Link href="/" >Dashboard</Link>
-                </div>
-                <div className="p-4 hover:bg-blue-800 w-60 ml-4 rounded flex">
-                    <Image src="/list.svg" width={15} height={15} />
-                    <Link href="/orders" >Orders</Link>
-                </div>
-                <div className="p-4 hover:bg-blue-800 w-60 ml-4 rounded flex">
-                    <Image src="/cart.svg" width={20} height={20} />
-                    <Link href="/shippings" >Shippings</Link>
-                </div>
-                <div className="p-4 hover:bg-blue-800 w-60 ml-4 rounded flex">
-                    <Image src="/support.svg" width={17} height={17} />
-                    <Link href="/support" >Support</Link>
-                </div>
-                <div className="p-4 hover:bg-blue-800 w-60 ml-4 mt-10 rounded flex">
-                    <Image src="/settings.svg" width={15} height={15} />
-                    <Link href="/settings" >Settings</Link>
-                </div>
+                <Link href="/" >
+                    <a className="p-4 hover:bg-blue-800 w-60 ml-4 rounded flex gap-10">
+                        <Image src="/dashboard.svg" width={15} height={15} />
+                        <span>Dashboard</span>
+                    </a>
+                </Link>
+                <Link href="/" >
+                    <a className="p-4 hover:bg-blue-800 w-60 ml-4 rounded flex gap-10">
+                        <Image src="/list.svg" width={15} height={15} />
+                        <span>Orders</span>
+                    </a>
+                </Link>
+                <Link href="/" >
+                    <a className="p-4 hover:bg-blue-800 w-60 ml-4 rounded flex gap-10">
+                        <Image src="/cart.svg" width={20} height={20} />
+                        <span>Shippings</span>
+                    </a>
+                </Link>
+                <Link href="/" >
+                    <a className="p-4 hover:bg-blue-800 w-60 ml-4 rounded flex gap-10">
+                        <Image src="/support.svg" width={17} height={17} />
+                        <span>Support</span>
+                    </a>
+                </Link>
+                <Link href="/" >
+                    <a className="p-4 hover:bg-blue-800 w-60 ml-4 rounded flex gap-10">
+                        <Image src="/settings.svg" width={15} height={15} />
+                        <span>Settings</span>
+                    </a>
+                </Link>
             </nav>
 
 
